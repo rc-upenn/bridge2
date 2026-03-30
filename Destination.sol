b28cf9a7ab29dffc03c83efccc0324544c9d7388
rc-upenn <rwille@seas.upenn.edu>
2026-03-30 16:27:18 +0100

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -23,7 +23,28 @@ contract Destination is AccessControl {
     }
 
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-		BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this));
+		address wrapped = underlying_tokens[_underlying_token];
+        require(wrapped != address(0), "Token not registered");
+
+        BridgeToken(wrapped).mint(_recipient, _amount);
+
+        emit Wrap(_underlying_token, wrapped, _recipient, _amount);
+	}
+
+	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
+		//YOUR CODE HERE
+    	address underlying = wrapped_tokens[_wrapped_token];
+        require(underlying != address(0), "Not a registered wrapped token");
+
+        // Burns from msg.sender (they must own the tokens)
+        BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
+
+        emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
+	}
+
+	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
+		//YOUR CODE HERE
+    	BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this));
 
         address wrapped = address(newToken);
 
@@ -34,27 +55,6 @@ contract Destination is AccessControl {
 
         emit Creation(_underlying_token, wrapped);
         return wrapped;
-	}
-
-	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-		//YOUR CODE HERE
-    address wrapped = underlying_tokens[_underlying_token];
-        require(wrapped != address(0), "Token not registered");
-
-        BridgeToken(wrapped).mint(_recipient, _amount);
-
-        emit Wrap(_underlying_token, wrapped, _recipient, _amount);
-	}
-
-	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-		//YOUR CODE HERE
-    address underlying = wrapped_tokens[_wrapped_token];
-        require(underlying != address(0), "Not a registered wrapped token");
-
-        // Burns from msg.sender (they must own the tokens)
-        BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
-
-        emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 }
